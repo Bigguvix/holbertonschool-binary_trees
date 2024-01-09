da49384e077587fe03f759dae0de74177a8e344b
Bigguvix <nahufeola@hotmail.com>
2024-01-09 08:52:24 -0800

    fixes

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -26,7 +26,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left->parent = n;
 	}
 
-	parent->left = node;
+	parent->left = n;
 
-	return (node);
+	return (n);
 }
