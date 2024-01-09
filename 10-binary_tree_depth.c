9364bad640069f7fc29df7944bfa59b75365baa3
Bigguvix <nahufeola@hotmail.com>
2024-01-09 09:53:41 -0800

    #10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures depth of node
+ *
+ * @tree: pointer to root
+ *
+ * Return: tree = NULL
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+}
