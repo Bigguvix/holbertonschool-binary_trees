fe81ad4c2f87d34efc47593b7212c5e2729bf518
Bigguvix <nahufeola@hotmail.com>
2024-01-09 09:28:32 -0800

    #4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,18 @@
+#include "binary_trees_h"
+
+/**
+ * binary_tree_is_leaf - checks if node is leaf.
+ *
+ * @node: pointer to node
+ *
+ * Return: 1 if node is leaf.
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL || node->left != NULL || node->right != NULL)
+	{
+		return (0);
+	}
+
+	return (1);
+}
